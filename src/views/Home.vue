4f9a4a4fdcdf760e4c5df54f6ae64652681a6f2b
teddinata <teddibreakthelimit@gmail.com>
2020-02-22 13:41:18 +0700

    upload projek vue

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+   <HeaderShayna />
+   <CarouselShayna />
+   <ProductShayna />
+    <InstaShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import CarouselShayna from '@/components/CarouselShayna.vue'
+import ProductShayna from '@/components/ProductShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    CarouselShayna,
+    ProductShayna,
+    InstaShayna,
+    FooterShayna
+}
+}
+</script>
